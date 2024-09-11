1e08860c884067886f9c6024e7f369bd703ad34d
nach0aguilar <nachoaguilarfdz@gmail.com>
2024-09-11 18:39:33 +0200

    Conditional removed in the 'upperFirstLetter' function because it would never receive an empty string as a parameter

--- src/components/DrinkList.tsx
+++ src/components/DrinkList.tsx
@@ -8,7 +8,6 @@ interface DrinkListProps {
 }
 
 function upperFirstLetter(word: string): string {
-  if (!word) return word;
   return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
 }
 
