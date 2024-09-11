64d4e37daf378d821b0dcfb5809d65d38bd548e7
nach0aguilar <nachoaguilarfdz@gmail.com>
2024-09-11 21:28:16 +0200

    Changed index to drink.id to iterate through objects by unique identifier instead of by index

--- src/components/DrinkList.tsx
+++ src/components/DrinkList.tsx
@@ -20,8 +20,8 @@ const DrinkList: React.FC<DrinkListProps> = ({ category }) => {
       <>
       <h2 className="text-4xl my-7 pl-5">{upperFirstLetter(category)}</h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 ml-10">
-        {filteredDrinks.map((drink, index) => (
-          <DrinkCard key={index} drink={drink} />
+        {filteredDrinks.map((drink) => (
+          <DrinkCard key={drink.id} drink={drink} />
         ))}
       </div>
       </>
--- src/data/drinks.ts
+++ src/data/drinks.ts
@@ -1,4 +1,5 @@
 export type Drink = {
+    id: number;
     category: string;
     name: string;
     img: string;
@@ -12,6 +13,7 @@ export type Drink = {
 export const DRINKS: Drink[] = [
     // Vodka
     {
+        id: 0,
         category: "Vodka",
         name: "Absolut",
         img: "https://benabola-skylounge-menu.herokuapp.com/img/drinks/vodka/absolut.webp",
@@ -22,6 +24,7 @@ export const DRINKS: Drink[] = [
         highlighted: true,
     },
     {
+        id: 1,
         category: "Vodka",
         name: "Ciroc",
         img: "https://benabola-skylounge-menu.herokuapp.com/img/drinks/vodka/ciroc.webp",
@@ -32,6 +35,7 @@ export const DRINKS: Drink[] = [
         highlighted: false,
     },
     {
+        id: 2,
         category: "Vodka",
         name: "Belvedere",
         img: "https://benabola-skylounge-menu.herokuapp.com/img/drinks/vodka/belvedere.webp",
@@ -43,6 +47,7 @@ export const DRINKS: Drink[] = [
     },
     // Gin
     {
+        id: 3,
         category: "Gin",
         name: "Beefeater",
         img: "https://benabola-skylounge-menu.herokuapp.com/img/drinks/gin/beefeater.webp",
@@ -53,6 +58,7 @@ export const DRINKS: Drink[] = [
         highlighted: false,
     },
     {
+        id: 4,
         category: "Gin",
         name: "Beefeater Pink",
         img: "https://benabola-skylounge-menu.herokuapp.com/img/drinks/gin/beefeater-pink.webp",
@@ -63,6 +69,7 @@ export const DRINKS: Drink[] = [
         highlighted: false,
     },
     {
+        id: 5,
         category: "Gin",
         name: "Bombay Sapphire",
         img: "https://benabola-skylounge-menu.herokuapp.com/img/drinks/gin/bombay-sapphire.webp",
@@ -73,6 +80,7 @@ export const DRINKS: Drink[] = [
         highlighted: false,
     },
     {
+        id: 6,
         category: "Gin",
         name: "Brockmans",
         img: "https://benabola-skylounge-menu.herokuapp.com/img/drinks/gin/brockmans.webp",
@@ -83,6 +91,7 @@ export const DRINKS: Drink[] = [
         highlighted: false,
     },
     {
+        id: 7,
         category: "Gin",
         name: "G'vine",
         img: "https://benabola-skylounge-menu.herokuapp.com/img/drinks/gin/gvine.webp",
@@ -93,6 +102,7 @@ export const DRINKS: Drink[] = [
         highlighted: false,
     },
     {
+        id: 8,
         category: "Gin",
         name: "Hendricks",
         img: "https://benabola-skylounge-menu.herokuapp.com/img/drinks/gin/hendricks.webp",
@@ -103,6 +113,7 @@ export const DRINKS: Drink[] = [
         highlighted: false,
     },
     {
+        id: 9,
         category: "Gin",
         name: "Jinzu",
         img: "https://benabola-skylounge-menu.herokuapp.com/img/drinks/gin/jinzu.webp",
@@ -113,6 +124,7 @@ export const DRINKS: Drink[] = [
         highlighted: false,
     },
     {
+        id: 10,
         category: "Gin",
         name: "Malfy Limone",
         img: "https://benabola-skylounge-menu.herokuapp.com/img/drinks/gin/malfy-limone.webp",
@@ -123,6 +135,7 @@ export const DRINKS: Drink[] = [
         highlighted: false,
     },
     {
+        id: 11,
         category: "Gin",
         name: "Martin Miller's",
         img: "https://benabola-skylounge-menu.herokuapp.com/img/drinks/gin/martin-millers.webp",
@@ -133,6 +146,7 @@ export const DRINKS: Drink[] = [
         highlighted: false,
     },
     {
+        id: 12,
         category: "Gin",
         name: "Monkey 47",
         img: "https://benabola-skylounge-menu.herokuapp.com/img/drinks/gin/monkey-47.webp",
@@ -143,6 +157,7 @@ export const DRINKS: Drink[] = [
         highlighted: true,
     },
     {
+        id: 13,
         category: "Gin",
         name: "Seagram's",
         img: "https://benabola-skylounge-menu.herokuapp.com/img/drinks/gin/seagrams.webp",
@@ -153,6 +168,7 @@ export const DRINKS: Drink[] = [
         highlighted: false,
     },
     {
+        id: 14,
         category: "Gin",
         name: "Tanqueray Ten",
         img: "https://benabola-skylounge-menu.herokuapp.com/img/drinks/gin/tanqueray-ten.webp",
@@ -164,6 +180,7 @@ export const DRINKS: Drink[] = [
     },
     // Whisky
     {
+        id: 15,
         category: "Whisky",
         name: "Ballantines",
         img: "https://benabola-skylounge-menu.herokuapp.com/img/drinks/whisky/ballantines.webp",
@@ -174,6 +191,7 @@ export const DRINKS: Drink[] = [
         highlighted: false,
     },
     {
+        id: 16,
         category: "Whisky",
         name: "Jameson",
         img: "https://benabola-skylounge-menu.herokuapp.com/img/drinks/whisky/jameson.webp",
@@ -184,6 +202,7 @@ export const DRINKS: Drink[] = [
         highlighted: false,
     },
     {
+        id: 17,
         category: "Whisky",
         name: "Chivas 12",
         img: "https://benabola-skylounge-menu.herokuapp.com/img/drinks/whisky/chivas-regal-12.webp",
@@ -194,6 +213,7 @@ export const DRINKS: Drink[] = [
         highlighted: false,
     },
     {
+        id: 18,
         category: "Whisky",
         name: "Chivas 18",
         img: "https://benabola-skylounge-menu.herokuapp.com/img/drinks/whisky/chivas-regal-18.webp",
@@ -204,6 +224,7 @@ export const DRINKS: Drink[] = [
         highlighted: false,
     },
     {
+        id: 19,
         category: "Whisky",
         name: "Jack Daniel's",
         img: "https://benabola-skylounge-menu.herokuapp.com/img/drinks/whisky/jack-daniels.webp",
@@ -214,6 +235,7 @@ export const DRINKS: Drink[] = [
         highlighted: false,
     },
     {
+        id: 20,
         category: "Whisky",
         name: "JW Red Label",
         img: "https://benabola-skylounge-menu.herokuapp.com/img/drinks/whisky/johnnie-walker-red.webp",
@@ -224,6 +246,7 @@ export const DRINKS: Drink[] = [
         highlighted: false,
     },
     {
+        id: 21,
         category: "Whisky",
         name: "JW Black Label",
         img: "https://benabola-skylounge-menu.herokuapp.com/img/drinks/whisky/johnnie-walker-black.webp",
@@ -234,6 +257,7 @@ export const DRINKS: Drink[] = [
         highlighted: false,
     },
     {
+        id: 22,
         category: "Whisky",
         name: "JW Gold Label",
         img: "https://benabola-skylounge-menu.herokuapp.com/img/drinks/whisky/johnnie-walker-gold.webp",
@@ -244,6 +268,7 @@ export const DRINKS: Drink[] = [
         highlighted: false,
     },
     {
+        id: 23,
         category: "Whisky",
         name: "Macallan 12",
         img: "https://benabola-skylounge-menu.herokuapp.com/img/drinks/whisky/macallan-12.webp",
@@ -254,6 +279,7 @@ export const DRINKS: Drink[] = [
         highlighted: false,
     },
     {
+        id: 24,
         category: "Whisky",
         name: "Maker's Mark",
         img: "https://benabola-skylounge-menu.herokuapp.com/img/drinks/whisky/makers-mark.webp",
@@ -265,6 +291,7 @@ export const DRINKS: Drink[] = [
     },
     // Rum
     {
+        id: 25,
         category: "Rum",
         name: "Bacardi",
         img: "https://benabola-skylounge-menu.herokuapp.com/img/drinks/rum/bacardi.webp",
@@ -275,6 +302,7 @@ export const DRINKS: Drink[] = [
         highlighted: true,
     },
     {
+        id: 26,
         category: "Rum",
         name: "Barceló Añejo",
         img: "https://benabola-skylounge-menu.herokuapp.com/img/drinks/rum/bacardi.webp",
@@ -285,6 +313,7 @@ export const DRINKS: Drink[] = [
         highlighted: false,
     },
     {
+        id: 27,
         category: "Rum",
         name: "Brugal",
         img: "https://benabola-skylounge-menu.herokuapp.com/img/drinks/rum/brugal.webp",
@@ -295,6 +324,7 @@ export const DRINKS: Drink[] = [
         highlighted: false,
     },
     {
+        id: 28,
         category: "Rum",
         name: "Captain Morgan",
         img: "https://benabola-skylounge-menu.herokuapp.com/img/drinks/rum/captain-morgan.webp",
@@ -305,6 +335,7 @@ export const DRINKS: Drink[] = [
         highlighted: false,
     },
     {
+        id: 29,
         category: "Rum",
         name: "Havana Club 7",
         img: "https://benabola-skylounge-menu.herokuapp.com/img/drinks/rum/havana-club-7.webp",
@@ -315,6 +346,7 @@ export const DRINKS: Drink[] = [
         highlighted: false,
     },
     {
+        id: 30,
         category: "Rum",
         name: "Havana Club Seleccion",
         img: "https://benabola-skylounge-menu.herokuapp.com/img/drinks/rum/havana-club-seleccion.webp",
@@ -325,6 +357,7 @@ export const DRINKS: Drink[] = [
         highlighted: false,
     },
     {
+        id: 31,
         category: "Rum",
         name: "Malibú",
         img: "https://benabola-skylounge-menu.herokuapp.com/img/drinks/rum/malibu.webp",
@@ -335,6 +368,7 @@ export const DRINKS: Drink[] = [
         highlighted: false,
     },
     {
+        id: 32,
         category: "Rum",
         name: "Kraken Black Spiced",
         img: "https://benabola-skylounge-menu.herokuapp.com/img/drinks/rum/kraken-black.webp",
